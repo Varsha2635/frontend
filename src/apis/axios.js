27e948ec614f716084e9c9b4f09c2a3f1235eafd
Varsha Kundarapu <123001134+Varsha2635@users.noreply.github.com>
2025-07-21 19:27:38 +0530

    Update axios.js

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://server1-beta.vercel.app/',
+  baseURL: 'https://server1-pjta76dum-varsha-kundarapus-projects.vercel.app/',
   withCredentials: true,
 });
 
